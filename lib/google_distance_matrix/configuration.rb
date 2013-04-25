2d96ec00212f8bf28d7521a615c5c1073ead5116
Thorbjørn Hermansen <thhermansen@gmail.com>
2013-04-25 10:17:35 +0200

    Configuration knows when it is valid or not.

--- lib/google_distance_matrix/configuration.rb
+++ lib/google_distance_matrix/configuration.rb
@@ -1,7 +1,16 @@
 class GoogleDistanceMatrix::Configuration
-  ATTRIBUTES = %w[sensor]
+  include ActiveModel::Validations
+
+  ATTRIBUTES = %w[sensor mode avoid units]
 
   attr_accessor *ATTRIBUTES
+
+
+  validates :sensor, inclusion: {in: [true, false]}
+  validates :mode, inclusion: {in: ["driving", "walking", "bicycling"]}, allow_blank: true
+  validates :avoid, inclusion: {in: ["tolls", "highways"]}, allow_blank: true
+  validates :units, inclusion: {in: ["metric", "imperial"]}, allow_blank: true
+
 
   def initialize
     self.sensor = false
--- spec/lib/google_distance_matrix/configuration_spec.rb
+++ spec/lib/google_distance_matrix/configuration_spec.rb
@@ -3,8 +3,25 @@ require "spec_helper"
 describe GoogleDistanceMatrix::Configuration do
   subject { described_class.new }
 
+  describe "Validations" do
+    it { should ensure_inclusion_of(:sensor).in_array([true, false]) }
+
+    it { should ensure_inclusion_of(:mode).in_array(["driving", "walking", "bicycling"]) }
+    it { should allow_value(nil).for(:mode) }
+
+    it { should ensure_inclusion_of(:avoid).in_array(["tolls", "highways"]) }
+    it { should allow_value(nil).for(:avoid) }
+
+    it { should ensure_inclusion_of(:units).in_array(["metric", "imperial"]) }
+    it { should allow_value(nil).for(:units) }
+  end
+
+
   describe "defaults" do
     its(:sensor) { should be_false }
+    its(:mode) { should be_nil }
+    its(:avoid) { should be_nil }
+    its(:units) { should be_nil }
   end
 
   describe "#to_hash" do
