0302c18e72ffc19a030aca72dd797b6572aaf683
Thorbjørn Hermansen <thhermansen@gmail.com>
2013-08-06 20:38:37 +0200

    ClientCache.key. Centralise knowledge on how to generate key.

--- lib/google_distance_matrix/client_cache.rb
+++ lib/google_distance_matrix/client_cache.rb
@@ -2,13 +2,17 @@ module GoogleDistanceMatrix
   class ClientCache
     attr_reader :client, :cache
 
+    def self.key(url)
+      url
+    end
+
     def initialize(client, cache)
       @client = client
       @cache = cache
     end
 
     def get(url, options = {})
-      cache.fetch url do
+      cache.fetch self.class.key(url) do
         client.get url, options
       end
     end
