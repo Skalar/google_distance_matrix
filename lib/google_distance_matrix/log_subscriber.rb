f998e02aaa0e990441ee4f52c156e6669c239e84
Thorbjørn Hermansen <thhermansen@gmail.com>
2015-01-05 22:19:04 +0100

    Fixed issue with argument error when we tried to log.
    Fixes #5. Hm, how did this even work before?

--- lib/google_distance_matrix/log_subscriber.rb
+++ lib/google_distance_matrix/log_subscriber.rb
@@ -1,14 +1,13 @@
 module GoogleDistanceMatrix
   class LogSubscriber < ActiveSupport::LogSubscriber
-    def self.logger
-      GoogleDistanceMatrix.logger
-    end
-
     def client_request_matrix_data(event)
       logger.info "(#{event.duration}ms) (elements: #{event.payload[:elements]}) GET #{event.payload[:url]}", tag: :client
+    end
+
+    def logger
+      GoogleDistanceMatrix.logger
     end
   end
 end
 
 GoogleDistanceMatrix::LogSubscriber.attach_to "google_distance_matrix"
-
