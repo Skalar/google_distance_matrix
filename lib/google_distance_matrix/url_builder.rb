b078d337639b2daa2ac465f820b55820392e8595
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-05-16 18:28:08 +0200

    Made UrlBuilder#places_to_param bit more readable.

--- lib/google_distance_matrix/url_builder.rb
+++ lib/google_distance_matrix/url_builder.rb
@@ -1,3 +1,5 @@
+require_relative 'url_builder/polyline_encoder_buffer'
+
 module GoogleDistanceMatrix
   class UrlBuilder
     BASE_URL = "maps.googleapis.com/maps/api/distancematrix/json"
@@ -48,7 +50,6 @@ module GoogleDistanceMatrix
     end
 
     def params
-
       configuration.to_param.merge(
         origins: places_to_param(matrix.origins),
         destinations: places_to_param(matrix.destinations)
@@ -59,24 +60,18 @@ module GoogleDistanceMatrix
       places_to_param_config = {lat_lng_scale: configuration.lat_lng_scale}
 
       out = []
-      encode_buffer = []
+      polyline_encode_buffer = PolylineEncoderBuffer.new
 
       places.each do |place|
         if place.lat_lng? && configuration.use_encoded_polylines
-          encode_buffer << place.lat_lng
+          polyline_encode_buffer << place.lat_lng
         else
-          if encode_buffer.any?
-            out << escape("enc:#{PolylineEncoder.encode encode_buffer}:")
-            encode_buffer.clear
-          end
+          polyline_encode_buffer.flush to: out
           out << escape(place.to_param places_to_param_config)
         end
       end
 
-      if encode_buffer.any?
-        out << escape("enc:#{PolylineEncoder.encode encode_buffer}:")
-        encode_buffer.clear
-      end
+      polyline_encode_buffer.flush to: out
 
       out.join(DELIMITER)
     end
