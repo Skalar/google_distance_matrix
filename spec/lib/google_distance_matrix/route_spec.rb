655fcca2eee0592f021094994be54fcb96a709e0
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-05-22 11:13:43 +0200

    Route class can contain duration_in_traffic data.

--- spec/lib/google_distance_matrix/route_spec.rb
+++ spec/lib/google_distance_matrix/route_spec.rb
@@ -5,6 +5,7 @@ describe GoogleDistanceMatrix::Route do
     {
       "distance" => {"text" => "2.0 km", "value" => 2032},
       "duration" => {"text" =>"6 mins",  "value" => 367},
+      "duration_in_traffic" => {"text" =>"5 mins",  "value" => 301},
       "status" =>"OK"
     }
   end
@@ -16,6 +17,8 @@ describe GoogleDistanceMatrix::Route do
   it { expect(subject.distance_text).to eq "2.0 km" }
   it { expect(subject.duration_in_seconds).to eq 367 }
   it { expect(subject.duration_text).to eq "6 mins" }
+  it { expect(subject.duration_in_traffic_in_seconds).to eq 301 }
+  it { expect(subject.duration_in_traffic_text).to eq "5 mins" }
 
   it { is_expected.to be_ok }
 end
